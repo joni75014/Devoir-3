fb7189749cad85009dc1bcf4feec36e68c191662
joni75014 <45243676+joni75014@users.noreply.github.com>
2019-11-27 17:31:04 +0100

    Ajoutez des fichiers projet.

---
+++ GestionnaireBDD/GstBdd.cs
@@ -0,0 +1,108 @@
+﻿using ClassesMetier;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GestionnaireBDD
+{
+    public class GstBdd
+    {
+        MySqlConnection cnx;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public GstBdd()
+        {
+            string driver = "server=localhost;user id=root;password=;database=devoir_reservation";
+            cnx = new MySqlConnection(driver);
+            cnx.Open();
+        }
+
+        public List<Manifestation> GetAllManifestations()
+        {
+            List<Manifestation> lesManifestations = new List<Manifestation>();
+            cmd = new MySqlCommand("select idManif, nomManif, dateDebut, dateFin,idSalle,numSAlle,nbPlaces from manifestation inner join salle  where numSalle=idSalle",cnx);
+        
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+
+                Salle uneSalle = new Salle()
+                {
+                    IdSalle = Convert.ToInt16(dr[4].ToString()),
+                    NomSalle = dr[5].ToString(),
+                    NbPlaces = Convert.ToInt16(dr[6].ToString()),
+                };
+
+             
+
+                Manifestation uneManifestation = new Manifestation()
+                {
+                    IdManif = Convert.ToInt16(dr[0].ToString()),
+                    NomManif = dr[1].ToString(),
+                    DateDebutManif = dr[2].ToString(),
+                    DateFinManif = dr[3].ToString(),
+                    LaSalle = uneSalle,
+                };
+                lesManifestations.Add(uneManifestation);              
+            }
+
+            dr.Close();
+            return lesManifestations;
+        }
+
+        public List<Place> GetAllPlacesByIdManifestation(int idManif, int idSalle)
+        {
+
+                 List<Place> lesPlaces = new List<Place>();
+                 cmd = new MySqlCommand("select idPlace, nbPlaces, nomSalle, libre, numPlace, numSalle, codeTarif from manifestation inner join occuper on idManif = numManif inner join place on numPlace = place.idPlace where idManif =" + idManif + " and place.numSalle =" + idSalle + ";", cnx);
+                 dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+               {
+                 Place unePlace = new Place()
+               {
+              IdPlace = Convert.ToInt16(dr[0].ToString()),
+              CodeTarif = Convert.ToChar(dr[1].ToString()),
+              Occupee = Convert.ToBoolean(dr[2].ToString()),
+
+             };
+                lesPlaces.Add(unePlace);
+
+                
+
+
+        }
+            dr.Close();
+            return lesPlaces;
+
+        }
+        public List<Tarif> GetAllTarifs()
+        {
+
+            List<Tarif> lesTarifs = new List<Tarif>();
+            cmd = new MySqlCommand("select idTarif, nomTarif, prix from tarif", cnx);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Tarif unTarif = new Tarif()
+                {
+                    IdTarif = Convert.ToChar(dr[0].ToString()),
+                    NomTarif = dr[1].ToString(),
+                    Prix = Convert.ToDouble(dr[2].ToString())
+                };
+                lesTarifs.Add(unTarif);
+            }
+            dr.Close();
+
+            return lesTarifs;
+      
+        }
+
+        public void ReserverPlace(int idPlace, int idSalle,int idManif)
+        {
+          
+        }
+    }
+}
---
+++ UWPGestionSalles/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GestionnaireBDD;
+using ClassesMetier;
+using Windows.UI.Popups;
+
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPGestionSalles
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        GstBdd bdd;
+
+        private async void btnReserver_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            bdd = new GstBdd();
+            lstManifs.ItemsSource = bdd.GetAllManifestations();
+            lstTarifs.ItemsSource = bdd.GetAllTarifs();
+        }
+
+        private void lstManifs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gvPlaces.ItemsSource = bdd.GetAllPlacesByIdManifestation((lstManifs.SelectedItem as Manifestation).IdManif, idSalle: (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle);
+        }
+
+        private async void gvPlaces_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (gvPlaces != null)
+            {
+                gvPlaces.ItemsSource = bdd.GetAllPlacesByIdManifestation((lstManifs.SelectedItem as Manifestation).IdManif, idSalle: (lstManifs.SelectedItem as Manifestation).LaSalle.IdSalle);
+            }
+        }
+    }
+}
